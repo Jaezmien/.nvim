6d2c998ef081d2a8e001cff155e493122c34e60d
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-26 20:38:39 +0800

    Cows are more important

--- after/plugin/duck.lua
+++ after/plugin/duck.lua
@@ -1,3 +1,3 @@
 local duck = require('duck')
-vim.keymap.set('n', '<leader>dd', function() duck.hatch("Ö") end, {})
+vim.keymap.set('n', '<leader>dd', function() duck.hatch("🐄") end, {})
 vim.keymap.set('n', '<leader>dk', function() duck.cook() end, {})
