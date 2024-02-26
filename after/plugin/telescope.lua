96f8bbfc10ecc1e80f56449170484a6561465658
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-26 22:49:16 +0800

    telescope: Add treesitter

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -14,8 +14,9 @@ require('telescope').setup{
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.treesitter, {})
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>pg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
