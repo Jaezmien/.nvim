0b2ef23a7a0385f7796e8959e786b3caa572599b
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-26 20:50:16 +0800

    Adjust background

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -1,14 +1,13 @@
 require('catppuccin').setup({
 	flavor = "mocha",
 	-- transparent_background = true,
-	transparent_background = false,
 })
 
 function SetupCatppuccin()
 	vim.cmd.colorscheme("catppuccin")
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 SetupCatppuccin()
