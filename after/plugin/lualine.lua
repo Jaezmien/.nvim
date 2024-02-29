299bcbe34595e726793cfa2da56b24c243cf5aca
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-29 19:52:07 +0800

    lualine: Use nerd font

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,9 +1,9 @@
 require('lualine').setup{
 	options = {
-		icons_enabled = false,
+		icons_enabled = true,
 		theme = 'catppuccin',
 		component_separators = '|',
-    	section_separators = '',
+		section_separators = { left = '', right = '' },
 	},
 }
 
