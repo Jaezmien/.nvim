9ceb2ecfacc4f741c89e5d8c9a128288ac1fdc85
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-29 20:46:53 +0800

    lazy: Organize setup table

--- lua/twilight-sparkle/lazy.lua
+++ lua/twilight-sparkle/lazy.lua
@@ -12,22 +12,21 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 return require('lazy').setup({
-	{
-		"catppuccin/nvim",
-		name = "catppuccin"
-	},
+	-- REALLY (not) Important
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ 'tamton-aquib/duck.nvim' },
 
+	-- Status Line
+	{ 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+
+	-- Navigator
 	{
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		dependencies = { {'nvim-lua/plenary.nvim'} }
+		tag = '0.1.5', dependencies = { {'nvim-lua/plenary.nvim'} }
 	},
 
-	{
-		'nvim-treesitter/nvim-treesitter',
-		-- build = ':TSUpdate',
-	},
-
+	-- LSP-related
+	{ 'nvim-treesitter/nvim-treesitter', },
 	{
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v3.x',
@@ -44,49 +43,20 @@ return require('lazy').setup({
 			{'L3MON4D3/LuaSnip'},
 		}
 	},
+	-- LSP-related (Visual)
+	{ 'nvim-treesitter/nvim-treesitter-context', dependencies = { { 'nvim-treesitter/nvim-treesitter' } } }, -- Shows the current code context
+	{ 'windwp/nvim-ts-autotag', dependencies = { { 'nvim-treesitter/nvim-treesitter' } } },
 
-	{
-		'lukas-reineke/indent-blankline.nvim',
-		name = 'ibl'
-	},
-	{
-		'HiPhish/rainbow-delimiters.nvim'
-	},
+	-- QOL
+	{ 'numToStr/Comment.nvim', },
 
-	{
-		'nvim-treesitter/nvim-treesitter-context',
-		dependencies = { { 'nvim-treesitter/nvim-treesitter' } }
-	},
-	{
-		'windwp/nvim-ts-autotag',
-		dependencies = { { 'nvim-treesitter/nvim-treesitter' } }
-	},
+	-- Pretty Visuals
+	{ 'lukas-reineke/indent-blankline.nvim', name = 'ibl' },
+	{ 'HiPhish/rainbow-delimiters.nvim' },
 	{
 		'windwp/nvim-autopairs',
 		event = "InsertEnter",
-		config = function()
-			require('nvim-autopairs').setup{}
-		end,
+		config = function() require('nvim-autopairs').setup{} end,
 	},
-
-	{
-		'numToStr/Comment.nvim',
-	},
-
-	{
-		'norcalli/nvim-colorizer.lua',
-		config = function()
-			require('colorizer').setup{}
-		end,
-	},
-
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' }
-	},
-
-	-- HIGHLY important
-	{
-		'tamton-aquib/duck.nvim'
-	}
+	{ 'norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup{} end, },
 })
