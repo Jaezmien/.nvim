8899e44ee33feec696bf9fdc6220ce0ccaa15ee0
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-29 20:03:39 +0800

    telescope: Enable git files keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -15,7 +15,7 @@ require('telescope').setup{
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.treesitter, {})
--- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
