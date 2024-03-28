54ebcd96d009dc387d74967a3161ca0b095b62a7
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-03-28 21:33:14 +0800

    lsp: Turn into one line

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,11 +21,7 @@ cmp.setup({
 		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 		['<C-y>'] = cmp.mapping.confirm({ select = true }),
 		["<C-Space>"] = cmp.mapping.complete(),
-		["<Tab>"] = cmp.mapping(
-			function (fallback)
-				fallback()
-			end, { "i", "s" }
-		),
+		["<Tab>"] = cmp.mapping( function (fallback) fallback() end, { "i", "s" }),
 		["<S-Tab>"] = nil,
 	}),
 	snippet = {
