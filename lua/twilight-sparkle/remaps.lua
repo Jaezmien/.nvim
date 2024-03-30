f466a5bdec71420f704146511dadbf65c9e5e4af
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-03-30 13:36:39 +0800

    remaps: Add window split shortcuts

--- lua/twilight-sparkle/remaps.lua
+++ lua/twilight-sparkle/remaps.lua
@@ -39,3 +39,12 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- It's enough
 vim.keymap.set("n", "<leader>bp", ":bp<CR>")
 vim.keymap.set("n", "<leader>bn", ":bn<CR>")
+
+-- Window split shortcuts
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set('n', '<leader>wv', '<C-W>s')
+vim.keymap.set('n', '<leader>ws', '<C-W>v')
+vim.keymap.set('n', '<leader>wq', '<C-W>q')
