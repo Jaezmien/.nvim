7985a4e8e340e605c02c931827bb2e255dbd4cda
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-04-05 22:22:47 +0800

    nvim-tree: Adjust config

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,9 +1,15 @@
 require('nvim-tree').setup({
 	actions = {
 		open_file = {
-			quit_on_open = true
+			quit_on_open = true,
+			window_picker = {
+				enable = false
+			}
 		}
-	}
+	},
+	view = {
+		width = 35,
+	},
 })
 
 vim.keymap.set("n", "<leader>pv", '<cmd>NvimTreeFindFile<CR>', { desc = "[P]roject [V]iew" })
