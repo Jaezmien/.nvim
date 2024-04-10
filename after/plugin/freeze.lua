0ef5eb8c43e320f626be8087f93f947ecd28b3fb
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-04-10 13:45:19 +0800

    freeze: Update config

--- after/plugin/freeze.lua
+++ after/plugin/freeze.lua
@@ -1,11 +1,14 @@
 require('charm-freeze').setup({
 	command = "freeze",
 	output = function()
-		return "./" .. os.date("%Y-%m-%d") .. "_freeze.webp"
+		return "./" .. os.date("%Y-%m-%d") .. "_freeze.png"
 	end,
 	theme = "catppuccin-mocha",
 	window = true,
-	show_line_numbers = true
+	show_line_numbers = true,
+	font = {
+		family = "ProggyClean Nerd Font Mono"
+	}
 })
 
 vim.keymap.set('v', '<leader>sc', '<cmd>Freeze<cr>', { desc = "[S]creenshot [C]ode" })
