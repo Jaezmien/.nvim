8e04e4956c37df7bc221af85ff28cf6c154456d7
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-04-10 13:47:10 +0800

    nvim: Add neovide config

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -9,11 +9,13 @@ require('catppuccin').setup({
 	}
 })
 
-function SetupCatppuccin()
-	vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("catppuccin")
 
+if vim.g.neovide then
+	local colors = require("catppuccin.palettes").get_palette("mocha")
+	vim.api.nvim_set_hl(0, "Normal", { bg = colors.base })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = colors.mantle })
+else
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
-
-SetupCatppuccin()
---
+++ after/plugin/neovide.lua
@@ -0,0 +1,6 @@
+if vim.g.neovide then
+	vim.o.guifont = "ProggyClean Nerd Font Mono:h12"
+
+	vim.g.neovide_cursor_trail_size = 0.8
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+end
