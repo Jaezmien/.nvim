ce14d14c8c9f04d591cfdadc1a8fb19aa8fa95d2
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-07-24 16:15:28 +0800

    nvim: Add window resize keybind

--- lua/twilight-sparkle/remaps.lua
+++ lua/twilight-sparkle/remaps.lua
@@ -37,14 +37,18 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>bp", ":bp<CR>", { desc = "Move [B]uffer [P]revious" })
 vim.keymap.set("n", "<leader>bn", ":bn<CR>", { desc = "Move [B]uffer [N]ext" })
 
--- Window split shortcuts
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- Window custom keybinds
+vim.keymap.set("n", "<C-h>", "10<C-w>h")
+vim.keymap.set("n", "<C-j>", "10<C-w>j")
+vim.keymap.set("n", "<C-k>", "10<C-w>k")
+vim.keymap.set("n", "<C-l>", "10<C-w>l")
 vim.keymap.set('n', '<leader>wv', '<C-W>s<C-w>j', { desc = "[W]indow Split [V]ertical"})
 vim.keymap.set('n', '<leader>w<S-v>', '<C-W>v<C-w>l', { desc = "[W]indow Split [H]orizontal"})
 vim.keymap.set('n', '<leader>wq', '<C-W>q', { desc = "[W]indow [Q]uit"})
+vim.keymap.set("n", "<A-h>", "10<C-w><")
+vim.keymap.set("n", "<A-j>", "10<C-w>+")
+vim.keymap.set("n", "<A-k>", "10<C-w>-")
+vim.keymap.set("n", "<A-l>", "10<C-w>>")
 
 -- Wrap toggle
 vim.keymap.set('n', '<leader>tw', function()
