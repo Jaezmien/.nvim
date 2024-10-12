cec1f28edec11a7917245cd69f6a423f3fc4d40a
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-10-12 18:26:07 +0800

    lualine: Use rounded separators

--- lua/twilight-sparkle/plugins/lualine.lua
+++ lua/twilight-sparkle/plugins/lualine.lua
@@ -5,8 +5,8 @@ return {
 		options = {
 			icons_enabled = true,
 			theme = 'catppuccin',
-			component_separators = '|',
-			section_separators = { left = '', right = '' },
+			component_separators = '│',
+			section_separators = { left = '', right = '' },
 		},
 	},
 	init = function()
