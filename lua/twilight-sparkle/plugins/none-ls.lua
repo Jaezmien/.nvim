7fb541f998d4d935096c6f147d7d68b0256783de
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2025-02-14 20:21:51 +0800

    nvim: Run code actions when formatting javascript/typescript files

--- lua/twilight-sparkle/plugins/none-ls.lua
+++ lua/twilight-sparkle/plugins/none-ls.lua
@@ -1,14 +1,14 @@
-return  {
+return {
 	'nvimtools/none-ls.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local null = require('null-ls')
 
-		null.setup{
+		null.setup {
 			sources = {
 				null.builtins.formatting.prettier.with({
 					filetypes = {
-						"javascript","typescript","css","scss","html","json","yaml","markdown","graphql","md","txt",
+						"javascript", "typescript", "css", "scss", "html", "json", "yaml", "markdown", "graphql", "md", "txt",
 					},
 				}),
 			}
@@ -17,6 +17,15 @@ return  {
 	init = function()
 		vim.keymap.set('n', '<leader>ff', function()
 			vim.lsp.buf.format()
+
+			-- XXX: What???
+			local js = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }
+			if vim.tbl_contains(js, vim.bo.filetype) then
+				for _, action in ipairs({'source.removeUnusedImports', 'source.addMissingImports'}) do
+					vim.lsp.buf.code_action({ apply = true, context = { only = { action } } })
+				end
+			end
+
 			vim.cmd('w')
 		end, { desc = "[F]ormat [F]ile" })
 	end,
