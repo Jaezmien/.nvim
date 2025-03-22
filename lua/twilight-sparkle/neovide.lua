cd6476324f4c61f756ba83d26ed53d88a4025fd9
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2025-03-22 19:11:11 +0800

    neovide: Change font size

--- lua/twilight-sparkle/neovide.lua
+++ lua/twilight-sparkle/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-	vim.o.guifont = "JetbrainsMono Nerd Font:h10"
+	vim.o.guifont = "JetbrainsMono Nerd Font:h8"
 
 	vim.g.neovide_cursor_trail_size = 0.8
 	vim.g.neovide_cursor_vfx_mode = "pixiedust"
