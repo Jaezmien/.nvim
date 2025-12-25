2558ef92bf89229fe8483eeacc855f1a235897cc
Jaezmien Naejara <moo@jaezmien.com>
2025-12-25 23:25:42 +0800

    feat: make which key load faster

--- lua/fluttershy/plugins/qol/which-key.lua
+++ lua/fluttershy/plugins/qol/which-key.lua
@@ -1,6 +1,12 @@
 return {
 	'folke/which-key.nvim',
-	keys = { "<leader>" },
+	
+	-- This would be cool if it wasn't slow on first activation
+	-- keys = { "<leader>" },
+	
+	-- Slower, but at least we can immediately utilize it
+	event = "UIEnter",
+
 	init = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 300
