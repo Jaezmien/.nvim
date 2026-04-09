9f5b7a9d091b5f53d5cac0a2a6512c1ecbed0b7d
Jaezmien Naejara <moo@jaezmien.com>
2026-04-09 12:07:47 +0800

    fix: use new catppuccin colorscheme name

--- lua/fluttershy/plugins/style/lualine.lua
+++ lua/fluttershy/plugins/style/lualine.lua
@@ -5,7 +5,7 @@ return {
 	opts = {
 		options = {
 			icons_enabled = true,
-			theme = 'catppuccin',
+			theme = 'catppuccin-nvim',
 			component_separators = '│',
 			section_separators = { left = '', right = '' },
 		},
